c5d7317cd653bbf34f6b601bad00caea0286e00a
Boglarka1212 <kazmer.boglarka@gmail.com>
2026-03-06 19:24:43 +0100

    højre knap lavet

--- index.js
+++ index.js
@@ -38,3 +38,20 @@ document.addEventListener("keydown", function (e) {
     moveDodgerLeft();
   }
 });
+
+
+//knap til højre
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const right = parseInt(rightNumbers, 10);
+
+  if (right < 360) {
+    dodger.style.left = `${right + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
